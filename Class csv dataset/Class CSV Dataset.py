2b832b8d22edce2f1e9449f7f5bc45bf43249d58
Henrike Schwenn <henrike.schwenn@gmail.com>
2021-04-06 20:07:19 +0200

    Pseudocode included

--- Class csv dataset/Class CSV Dataset.py
+++ Class csv dataset/Class CSV Dataset.py
@@ -1,4 +1,46 @@
+import os
 import sys
-print(sys.executable)
 import numpy
 import pandas
+
+class csv_object:
+    def get_csv_dataset():
+        os.chdir('''path-csv-dataset''')
+
+
+"""Creates a dataset object from a csv file
+
+
+What it's gonna do
+
+
+- Get a csv file from a directory
+        - Instance variable: "path-csv-dataset"
+        - Instance variable: "csv-dataset-object"
+        - get_dataset():
+            os.chdir(path-csv-dataset)
+    - Verify if the format is actually csv
+        - If yes, proceed
+        - If no, print error message: "Please select a .csv file"
+- Return a file object
+        - csv_object = get_dataset()
+            - Name the object via raw input
+            
+- Display the object
+    - display_dataset = read_csv()
+
+**Needs:**
+
+- os
+- numpy
+- pandas
+
+**Instance variables:**
+
+- path to data file
+- name of data file
+
+**Methods:**
+
+- get_dataset = os.chdir(path)
+- display_dataset = read_csv()"""
