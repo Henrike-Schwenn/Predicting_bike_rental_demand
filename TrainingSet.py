499069469d8e2090a2f2f7d97e94e4848c42d834
henrike-94 <henrike.schwenn@gmx.de>
2021-07-26 18:18:57 +0200

    Dependent variable "rent_count" replaced by natural log

--- TrainingSet.py
+++ TrainingSet.py
@@ -2,7 +2,19 @@ import sys
 import os
 import pandas
 import numpy
+sys.path.append("C:/Users/henri/PycharmProjects")
+import fastai
 
-trainingSetFirstCycle=pandas.read_csv("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/Datasets/train.csv")
-print(trainingSetFirstCycle)
+# Create a dataframe
+trainingSetFirstCycle=pandas.read_csv("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/Datasets/train.csv", low_memory=False, parse_dates=["datetime"])
+# Calculate the log for each value in dependent variable "count"
+# Each value in "rent_count" is being replaced by its log natural logarithm by defining the column a a variable and performing numpy.log on it
+trainingSetFirstCycle.rent_count=numpy.log(trainingSetFirstCycle.rent_count)
+# TODO Split "datetime" into several numerical columns
+# Use fastai function "add_datepart"
+# TODO Check missing values
+# TODO Add missing numeric values
+# "proc_DF"
+# TODO Move dependent variable into a separate variable
+# TODO Save dataframe into feather format
 
