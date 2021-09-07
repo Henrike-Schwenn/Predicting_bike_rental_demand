b16cba874fd1b8f3d5ce2d9bfc4b5468ceda9b1c
henrike-94 <henrike.schwenn@gmx.de>
2021-09-07 19:33:21 +0200

    Function 'add_datepart' included

--- TrainingSet.py
+++ TrainingSet.py
@@ -2,19 +2,24 @@ import sys
 import os
 import pandas
 import numpy
-sys.path.append("C:/Users/henri/PycharmProjects")
+import torch
 import fastai
 
 # Create a dataframe
+from fastai.tabular.core import add_datepart
+
 trainingSetFirstCycle=pandas.read_csv("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/Datasets/train.csv", low_memory=False, parse_dates=["datetime"])
 # Calculate the log for each value in dependent variable "count"
 # Each value in "rent_count" is being replaced by its log natural logarithm by defining the column a a variable and performing numpy.log on it
 trainingSetFirstCycle.rent_count=numpy.log(trainingSetFirstCycle.rent_count)
 # TODO Split "datetime" into several numerical columns
 # Use fastai function "add_datepart"
+trainingSetFirstCycle=add_datepart(trainingSetFirstCycle, datetime, drop=True)
+# TODO Check column name "datetime" (name 'datetime' is not defined)
 # TODO Check missing values
 # TODO Add missing numeric values
 # "proc_DF"
 # TODO Move dependent variable into a separate variable
 # TODO Save dataframe into feather format
+
 
