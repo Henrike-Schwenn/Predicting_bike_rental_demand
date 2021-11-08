a7bc2d931ca22e12e3b3d43bab9bc204798a0040
henrike-94 <henrike.schwenn@gmx.de>
2021-11-08 16:53:07 +0100

    "datetime" split into "date" and "time"

--- TrainingSet.py
+++ TrainingSet.py
@@ -8,13 +8,13 @@ from fastai.tabular.core import add_datepart
 
 # Create a dataframe
 trainingSetFirstCycle = pandas.read_csv(
-    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/Datasets/train_datetime_split.csv",
-    low_memory=False, parse_dates=["date"])
+    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/Datasets/train.csv",
+    low_memory=False, parse_dates=["datetime"])
 # Calculate the log for each value in dependent variable "count"
 # Each value in "rent_count" is being replaced by its log natural logarithm by defining the column a a variable and performing numpy.log on it
 trainingSetFirstCycle.rent_count = numpy.log(trainingSetFirstCycle.rent_count)
 # Use fastai function "add_datepart"
-trainingSetFirstCycle = add_datepart(trainingSetFirstCycle, "date", drop=True)
+trainingSetFirstCycle = add_datepart(trainingSetFirstCycle, "datetime", drop=True)
 # TODO Check missing values
 # TODO Add missing numeric values
 # "proc_DF"
