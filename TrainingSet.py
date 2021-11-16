06b657c3de043c604abc6f34fe485af87e940b08
henrike-94 <henrike.schwenn@gmx.de>
2021-11-16 16:58:04 +0100

    Training and validation sets completed, except for dependent variable in validation set

--- TrainingSet.py
+++ TrainingSet.py
@@ -4,19 +4,36 @@ import pandas
 import numpy
 import torch
 import fastai
-from fastai.tabular.core import add_datepart
-
+from fastai.data.transforms import RandomSplitter
+from fastai.tabular.core import add_datepart, FillMissing, Normalize, Categorify, TabularPandas
+from fastcore.basics import range_of
+import feather
 # Create a dataframe
-trainingSetFirstCycle = pandas.read_csv(
+dfFirstCycle = pandas.read_csv(
     "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/Datasets/train.csv",
     low_memory=False, parse_dates=["datetime"])
 # Calculate the log for each value in dependent variable "count"
 # Each value in "rent_count" is being replaced by its log natural logarithm by defining the column a a variable and performing numpy.log on it
-trainingSetFirstCycle.rent_count = numpy.log(trainingSetFirstCycle.rent_count)
+dfFirstCycle.rent_count = numpy.log(dfFirstCycle.rent_count)
 # Use fastai function "add_datepart"
-trainingSetFirstCycle = add_datepart(trainingSetFirstCycle, "datetime", drop=True)
-# TODO Check missing values
-# TODO Add missing numeric values
-# "proc_DF"
-# TODO Move dependent variable into a separate variable
-# TODO Save dataframe into feather format
+dfFirstCycle = add_datepart(dfFirstCycle, "datetime", drop=True)
+# Split dfFirstCycle in half. The first 5443 rows are the training set, the second are the validation set.
+dfFirstCycle_train, dfFirstCycle_validate = dfFirstCycle.iloc[:5443].copy(),dfFirstCycle.iloc[5443:].copy()
+# Save dependent variable into an array
+y_name_validation = dfFirstCycle_validate.rent_coun
+#TODO Transform y_name_validation to pd.dataframe
+#TODO Save y_name_validation into feather format
+#y_name_validation.to_feather("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/y_name_validation.ftr")
+# Drop dependent variable from the validation set
+dfFirstCycle_validate.drop('rent_count', axis=1, inplace=True)
+# Define categorical variables
+cat_names = ['season', 'holiday', 'workingday', 'weather', 'datetimeIs_month_end','datetimeIs_month_start','datetimeIs_quarter_end', 'datetimeIs_quarter_start', 'datetimeIs_year_end', 'datetimeIs_year_start']
+# Define continuous variables
+cont_names = ['temp', 'atemp', 'humidity', 'windspeed', 'casual', 'registered', 'rent_count']
+# Clean the data: Transform the categorical variables to something similar to pd.Categorical. Fill the missing values in continuous columns. Turn values into normal distribution.
+procs = [Categorify, FillMissing, Normalize]
+# Define splits
+splits = RandomSplitter()(range_of(dfFirstCycle_train))
+trainingFirstCycle = TabularPandas(dfFirstCycle_train, procs, cat_names, cont_names, y_names="rent_count", splits=splits)
+# TODO Save dataframes into feather format
+dfFirstCycle_train.to_feather("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/trainingFirstCycle")
