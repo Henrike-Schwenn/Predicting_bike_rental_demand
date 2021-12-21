b058d7cfc6c01d6150e549b0779ded6335510110
henrike-94 <henrike.schwenn@gmx.de>
2021-12-21 19:26:43 +0100

    Set up Training file, added validation set as csv

---
+++ FirstCycle/Train.py
@@ -0,0 +1,21 @@
+# Train Random Forest Regressor on dataframe trainingFirstCycle
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+import feather
+# TODO Read feather formatted trainingFirstCycle and define as array X_train
+    # This is the training set
+# TODO Define X_train.rent_count as dependent variable Y_train
+    # This is the dependent variable of the training set, the column to be predicted
+# TODO Read feather formatted dfFirstCycle_validate and define as array X_validate
+    # This is the name of the validation set
+# TODO Read y_name_validation.csv and define as Y_validate
+    # This is the dependent variable of the validation set, the column to be predicted
+# TODO Run sklearn.ensemble.RandomForestRegressor()
+# TODO Run fit method on training set
+# TODO Run print_score function on training and validation sets
+# TODO Return parameters
+    # fastai.print_score() takes care of that
+
+
+
+
--- FirstCycle/TrainingSet.py
+++ FirstCycle/TrainingSet.py
@@ -1,3 +1,4 @@
+import unittest
 import pandas
 import numpy
 import torch
@@ -6,6 +7,7 @@ from fastai.data.transforms import RandomSplitter
 from fastai.tabular.core import add_datepart, FillMissing, Normalize, Categorify, TabularPandas
 from fastcore.basics import range_of
 import feather
+
 # Create a dataframe
 dfFirstCycle = pandas.read_csv(
     "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/Datasets/train.csv",
@@ -16,20 +18,30 @@ dfFirstCycle.rent_count = numpy.log(dfFirstCycle.rent_count)
 # Use fastai function "add_datepart"
 dfFirstCycle = add_datepart(dfFirstCycle, "datetime", drop=True)
 # Split dfFirstCycle in half. The first 5443 rows are the training set, the second are the validation set.
-dfFirstCycle_train, dfFirstCycle_validate = dfFirstCycle.iloc[:5443].copy(),dfFirstCycle.iloc[5443:].copy()
+dfFirstCycle_train, dfFirstCycle_validate = dfFirstCycle.iloc[:5443].copy(), dfFirstCycle.iloc[5443:].copy()
 # Save dependent variable into an array
 y_name_validation = dfFirstCycle_validate.rent_count
-y_name_validation.to_csv("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/y_name_validation.csv")
+y_name_validation.to_csv(
+    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/y_name_validation.csv")
 # Drop dependent variable from the validation set
 dfFirstCycle_validate.drop('rent_count', axis=1, inplace=True)
 # Define categorical variables
-cat_names = ['season', 'holiday', 'workingday', 'weather', 'datetimeIs_month_end','datetimeIs_month_start','datetimeIs_quarter_end', 'datetimeIs_quarter_start', 'datetimeIs_year_end', 'datetimeIs_year_start']
+cat_names = ['season', 'holiday', 'workingday', 'weather', 'datetimeIs_month_end', 'datetimeIs_month_start',
+             'datetimeIs_quarter_end', 'datetimeIs_quarter_start', 'datetimeIs_year_end', 'datetimeIs_year_start']
 # Define continuous variables
 cont_names = ['temp', 'atemp', 'humidity', 'windspeed', 'casual', 'registered', 'rent_count']
 # Clean the data: Transform the categorical variables to something similar to pd.Categorical. Fill the missing values in continuous columns. Turn values into normal distribution.
 procs = [Categorify, FillMissing, Normalize]
 # Define splits
 splits = RandomSplitter()(range_of(dfFirstCycle_train))
-trainingFirstCycle = TabularPandas(dfFirstCycle_train, procs, cat_names, cont_names, y_names="rent_count", splits=splits)
+trainingFirstCycle = TabularPandas(dfFirstCycle_train, procs, cat_names, cont_names, y_names="rent_count",
+                                   splits=splits)
 # Save dataframes into feather format
-dfFirstCycle_train.to_feather("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/trainingFirstCycle")
+dfFirstCycle_train.to_feather(
+    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/trainingFirstCycle.feather")
+
+dfFirstCycle_validate.to_csv(
+    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/validateFirstCycle.csv")
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
