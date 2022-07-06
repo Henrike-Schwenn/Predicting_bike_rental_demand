6f2a9fd76b44589e5bf97e78da59f5fcf07f47b3
henrike-94 <henrike.schwenn@gmx.de>
2022-07-06 13:12:37 +0200

    Second Cycle Script "Train" running

--- Second Cycle/Train Second Cycle.py
+++ Second Cycle/Train Second Cycle.py
@@ -2,17 +2,21 @@
 
 import pandas
 import numpy
-import torch
-import fastai
-from fastai.data.transforms import RandomSplitter
+import sklearn
 from fastai.tabular.core import add_datepart, \
     FillMissing, Normalize, Categorify, TabularPandas
-from fastcore.basics import range_of
+from sklearn.ensemble import RandomForestRegressor
+import psutil
+import time
+import joblib
+
+start = time.process_time_ns()
 
 # Dataframe Training Set
 
-train_path = "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie"
-    "/Predicting_Bike_Rental_Demand/Datasets/train.csv"
+train_path = ("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg"
+              "/Sprachtechnologie/Predicting_Bike_Rental_Demand/Datasets"
+              "/train.csv")
 
 train_second_cycle = pandas.read_csv(
     train_path, low_memory=False, parse_dates=["datetime"])
@@ -31,7 +35,27 @@ cont_names = ['temp', 'atemp', 'humidity', 'windspeed', 'casual', 'registered',
 
 procs = [Categorify, FillMissing, Normalize]
 
+training_second_cycle = TabularPandas(train_second_cycle, procs, cat_names,
+                                      cont_names, y_names="rent_count")
 
 # Train Random Forest Regressor
+
+rf_2 = sklearn.ensemble.RandomForestRegressor()
+
+rf_2.fit(train_second_cycle, train_second_cycle.rent_count)
+
 # Save RF
+
+rf_2_trained_path = ("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg"
+                     "/Sprachtechnologie/Predicting_Bike_Rental_Demand"
+                     "/Datasets/rf_2_trained.joblib")
+
+rf_2_trained = joblib.dump(rf_2, rf_2_trained_path, compress=3)
+
 # Measure Performance
+
+end = time.process_time_ns()
+runtime = end - start
+
+print("Time elapsed:", runtime, "nanoseconds")
+print("RAM memory used:", psutil.virtual_memory()[2], "%")
