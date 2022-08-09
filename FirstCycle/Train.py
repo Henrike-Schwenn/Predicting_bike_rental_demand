b12fbc3a0dc488f2d4fb655c4aee36b1ae3311fa
henrike-94 <henrike.schwenn@gmx.de>
2022-08-09 11:51:49 +0200

    First Cycle edited

--- FirstCycle/Train.py
+++ FirstCycle/Train.py
@@ -1,73 +1,79 @@
 # Train Random Forest Regressor on dataframe trainingFirstCycle
-import os
 import psutil
 import math
 import time
-import matplotlib.pyplot
 import pandas
 import sklearn
-import fastai
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.datasets import make_regression
 from sklearn.metrics import mean_squared_error
 from sklearn.tree import export_graphviz
-import plotly.express
-import feather
 import unittest
 
-# Get the sum of the system and user CPU time of the current process in nanoseconds
-# Nanoseconds since the epoch, which is 1 January 1601, 00:00:00 for Windows
-# At the beginning of the process
+'''Get the sum of the system and user CPU time of the current
+ process in nanoseconds.
+Nanoseconds since the epoch, which is 1 January 1601, 00:00:00 for Windows
+at  the beginning of the process'''
 start = time.process_time_ns()
 
-# Read feather formatted trainingFirstCycle and define as array X_train.
+# Read feather formatted trainingFirstCycle and define as array x_train.
 # This is the training set
-X_train = pandas.read_feather(
-    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/"
-    "Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/trainingFirstCycle.feather")
-# Define X_train.rent_count as dependent variable Y_train.
-# This is the dependent variable of the training set, the column to be predicted.
-Y_train = X_train.rent_count
-# Visualize X_train and Y_train in a scatterplot
-# Some examples
-# Season=X_train.plot.scatter(x="season", y="rent_count")
-# Windspeed=X_train.plot.scatter(x="windspeed", y="rent_count")
-# Humidity=X_train.plot.scatter(x="humidity", y="rent_count")
-# Temperature=X_train.plot.scatter(x="temp", y="rent_count")
 
-# Read csv formatted validateFirstCycle and define as array X_validate.
-# This is the name of the validation set.
-X_validate = pandas.read_csv(
+train_path = (
+    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/"
+    "Sprachtechnologie/Predicting_Bike_Rental_Demand/"
+    "FirstCycle/trainingFirstCycle.feather")
+
+test_path = (
     "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie"
-    "/Predicting_Bike_Rental_Demand/FirstCycle/validateFirstCycle.csv")
-# Read y_name_validation.csv and define as Y_validate.
-# This is the dependent variable of the validation set, the column to be predicted.
-Y_validate = pandas.read_csv(
-    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie"
-    "/Predicting_Bike_Rental_Demand/FirstCycle/y_name_validation.csv")
+    "/Predicting_Bike_Rental_Demand/FirstCycle/testingFirstCycle.csv")
+
+y_test_path = ("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/"
+               "Sprachtechnologie/Predicting_Bike_Rental_Demand/"
+               "FirstCycle/y_name_validation.csv")
+
+x_train = pandas.read_feather(train_path)
+
+'''Define x_train.rent_count as dependent variable y_train.
+This is the dependent variable of the training set, the column to be 
+predicted.'''
+y_train = x_train.rent_count
+
+'''Read csv formatted testingFirstCycle and define as array x_test.
+This is the name of the test set.'''
+x_test = pandas.read_csv(test_path)
+
+'''Read y_name_test.csv and define as y_test.
+This is the dependent variable of the test set, the column to be predicted.'''
+y_test = pandas.read_csv(y_test_path)
 
 # Create sklearn.ensemble.RandomForestRegressor() object
 RF1 = sklearn.ensemble.RandomForestRegressor()
+
 # Run fit method on training set
-RF1.fit(X_train, Y_train)
+RF1.fit(x_train, y_train)
+
 # Visualize RF1.fit using export_graphviz
 # https://scikit-learn.org/stable/modules/generated/sklearn.tree.export_graphviz.html
 # Extract a single tree from the forest
 estimator = RF1.estimators_[5]
 RF1_sample = sklearn.tree.export_graphviz(estimator,
-                                          out_file="C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie"
-                                                   "/Predicting_Bike_Rental_Demand/FirstCycle/RF1.dot")
+                                          out_file="C:/Users/henri/OneDrive/"
+                                                   "Dokumente/Berufseinstieg/"
+                                                   "Sprachtechnologie"
+                                                   "/Predicting_Bike_Rental_Demand/"
+                                                   "FirstCycle/RF1.dot")
 
 # Predict y variable in test set.
-y_pred = RF1.predict(X_validate)
-# TODO Visualize prediction?
-y_true = Y_validate.rent_count.to_numpy()
+y_pred = RF1.predict(x_test)
+
+y_true = y_test.rent_count.to_numpy()
 
 # Evaluate prediction using root mean squared log error
 scoreRMSLE = math.sqrt(sklearn.metrics.mean_squared_log_error(y_true, y_pred))
-# TODO Visualize score
-# DataFrame.plot.scatter()
-# matplotlib.scatter(x,y)
+
+
+'''Saving as csv is not worth it: too much effort for a file that I couldn't
+upload to Kaggle anyway, because I used the wrong dataset for testing.'''
 
 # Return further parameters
 
@@ -76,11 +82,8 @@ scoreRMSLE = math.sqrt(sklearn.metrics.mean_squared_log_error(y_true, y_pred))
 end = time.process_time_ns()
 runtime = end - start
 
-# Code runs fine up to this point.
-
-
 print("Score RMSLE:", scoreRMSLE)
-print("Time elapsed:", runtime, "nanoseconds")
+print("Time elapsed:", runtime, "nanoseconds =", runtime*0.000000001, "seconds")
 print("RAM memory used:", psutil.virtual_memory()[2], "%")
 # TODO Visualize parameters
 # TODO Save all prints into a file
--- FirstCycle/TrainingSet.py
+++ FirstCycle/TrainingSet.py
@@ -1,61 +1,73 @@
 import unittest
 import pandas
 import numpy
-import torch
-import fastai
 from fastai.data.transforms import RandomSplitter
 from fastai.tabular.core import add_datepart, \
     FillMissing, Normalize, Categorify, TabularPandas
 from fastcore.basics import range_of
-import feather
 
 # Create a dataframe
-dfFirstCycle = pandas.read_csv(
-    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie"
-    "/Predicting_Bike_Rental_Demand/Datasets/train.csv",
-    low_memory=False, parse_dates=["datetime"])
-# Calculate the log for each value in dependent variable "count"
-'''Each value in "rent_count" is being replaced by its log natural logarithm by
+
+df_path = ("C:/Users/henri/OneDrive/Dokumente/Berufseinstieg"
+           "/Sprachtechnologie/Predicting_Bike_Rental_Demand/"
+           "Datasets/train.csv")
+
+df_first_cycle = pandas.read_csv(
+    df_path, low_memory=False, parse_dates=["datetime"])
+
+df_first_cycle = add_datepart(df_first_cycle, "datetime", drop=True)
+
+'''Calculate the log for each value in dependent variable "count"
+Each value in "rent_count" is being replaced by its log natural logarithm by
  defining the column a a variable and performing numpy.log on it'''
-dfFirstCycle.rent_count = numpy.log(dfFirstCycle.rent_count)
-# Use fastai function "add_datepart"
-dfFirstCycle = add_datepart(dfFirstCycle, "datetime", drop=True)
-# Split dfFirstCycle in half.
+df_first_cycle.rent_count = numpy.log(df_first_cycle.rent_count)
+
+# Split train_first_cycle in half.
 # The first 5443 rows are the training set, the second are the test set.
-dfFirstCycle_train, dfFirstCycle_validate = dfFirstCycle.iloc[
-                                            :5443].copy(), dfFirstCycle.iloc[
+df_first_cycle_train, df_first_cycle_test = df_first_cycle.iloc[
+                                            :5443].copy(), df_first_cycle.iloc[
                                                            5443:].copy()
 # Save dependent variable into an array
-y_name_validation = dfFirstCycle_validate.rent_count
-y_name_validation.to_csv(
+y_name_test = df_first_cycle_test.rent_count
+y_name_test.to_csv(
     "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/Sprachtechnologie"
-    "/Predicting_Bike_Rental_Demand/FirstCycle/y_name_validation.csv")
-# Drop dependent variable from the validation set
-dfFirstCycle_validate.drop('rent_count', axis=1, inplace=True)
+    "/Predicting_Bike_Rental_Demand/FirstCycle/y_name_test.csv")
+
+# Drop dependent variable from the test set
+df_first_cycle_test.drop('rent_count', axis=1, inplace=True)
+
 # Define categorical variables
 cat_names = ['season', 'holiday', 'workingday', 'weather',
              'datetimeIs_month_end', 'datetimeIs_month_start',
              'datetimeIs_quarter_end', 'datetimeIs_quarter_start',
              'datetimeIs_year_end', 'datetimeIs_year_start']
+
 # Define continuous variables
 cont_names = ['temp', 'atemp', 'humidity', 'windspeed', 'casual', 'registered',
               'rent_count']
-# Clean the data: Transform the categorical variables to something similar to pd.Categorical.
-# Fill the missing values in continuous columns. Turn values into normal distribution.
+
+'''Clean the data: Transform the categorical variables to something 
+similar to pd.Categorical.
+Fill the missing values in continuous columns.
+Turn values into normal distribution.'''
 procs = [Categorify, FillMissing, Normalize]
+
 # Define splits
-splits = RandomSplitter()(range_of(dfFirstCycle_train))
-trainingFirstCycle = TabularPandas(dfFirstCycle_train, procs, cat_names,
+splits = RandomSplitter()(range_of(df_first_cycle_train))
+trainingFirstCycle = TabularPandas(df_first_cycle_train, procs, cat_names,
                                    cont_names, y_names="rent_count",
                                    splits=splits)
-# Save dataframes into feather format
-dfFirstCycle_train.to_feather(
-    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/"
-    "Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/trainingFirstCycle.feather")
 
-dfFirstCycle_validate.to_csv(
+# Save dataframes
+df_first_cycle_train.to_feather(
+    "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg/"
+    "Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/"
+    "trainingFirstCycle.feather")
+
+df_first_cycle_test.to_csv(
     "C:/Users/henri/OneDrive/Dokumente/Berufseinstieg"
-    "/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle/validateFirstCycle.csv")
+    "/Sprachtechnologie/Predicting_Bike_Rental_Demand/FirstCycle"
+    "/testingFirstCycle.csv")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
